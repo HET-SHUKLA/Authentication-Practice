89035f0a98ed091e26bada5b0355e4d4d1b95024
HET SHUKLA <SHUKLAHET2704@gmail.com>
2024-11-09 13:43:01 +0530

    create requried files

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import connect from './connection.js';
+import {MONGO_URL, PORT} from './config.js';
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log(`Server Started`);
+app.use(express.urlencoded({extended: false}));
+
+//Routes
+app.use('/api/v1/signup', );
+app.use('/api/v1/signin', );
+app.use('/api/v1/logout', );
+
+connect(MONGO_URL).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server Started`);
+    });
+}).catch((e) => {
+    console.error(`Unable to connect to the Database : ${e}`);
 });
